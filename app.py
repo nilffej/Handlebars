f42e815ad220d877d36d9416a24450a0ae5113f7
Jeff Lin <jeff.andrew.lin@gmail.com>
2019-11-15 15:28:40 -0500

    Progress on search bar. Currently only adapted for MetaWeather API.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-# Team Mediocrity
+# Team Handlebars
 # SoftDev1 PD 9
 # K25
 # 11/13/2019
@@ -9,16 +9,29 @@ app = Flask(__name__)
 
 @app.route("/")
 def root():
-    u = urllib.request.urlopen("https://www.metaweather.com/api/location/44418/")
-    response = u.read()
-    data = json.loads(response)
-    weather = data['consolidated_weather'][0]
-    return render_template("homepage.html",
-                            place = data['title'],
-                            latt_long = data['latt_long'],
-                            applicable_date = weather['applicable_date'],
-                            weather_state_name = weather['weather_state_name'],
-                            image = "https://www.metaweather.com/static/img/weather/png/64/{}.png".format(weather['weather_state_abbr']))
+    return render_template("homepage.html")
+
+@app.route("/search")
+def search():
+    if request.args["searchbar"]:
+        u = urllib.request.urlopen("https://www.metaweather.com/api/location/search/?query={}".format(request.args["searchbar"].replace(" ","%20")))
+        response = u.read()
+        data = json.loads(response)
+        if len(data) == 0:
+            return redirect(url_for("root"))
+        u = urllib.request.urlopen("https://www.metaweather.com/api/location/{}".format(data[0]["woeid"]))
+        response = u.read()
+        data = json.loads(response)
+        weather = data['consolidated_weather'][0]
+        return render_template("searchresults.html", place = data['title'],
+                                latt_long = data['latt_long'],
+                                applicable_date = weather['applicable_date'],
+                                weather_state_name = weather['weather_state_name'],
+                                image = "https://www.metaweather.com/static/img/weather/png/64/{}.png".format(weather['weather_state_abbr']))
+    else:
+        return redirect(url_for("root"))
+
+
 
 if __name__ == "__main__":
     app.debug = True
