3bdf592f695c0274c87bfc522aad169ca89ee7ff
Henry Liu <hliu01@stuy.edu>
2019-11-19 14:43:10 -0500

    changed databases

--- app.py
+++ app.py
@@ -17,13 +17,16 @@ db = sqlite3.connect(DB_FILE)
 c = db.cursor()
 c.execute(''' SELECT count(name) FROM sqlite_master WHERE type='table' AND name='USERNAMES' ''')
 if c.fetchone()[0] < 1:
-    c.execute("CREATE TABLE userdata (userID INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, password TEXT);")
+    c.execute("CREATE TABLE USERNAMES(userID INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, password TEXT);")
+c.execute(''' SELECT count(name) FROM sqlite_master WHERE type='table' AND name='SAVED' ''')
+if c.fetchone()[0] < 1:
+    c.execute("CREATE TABLE SAVED(userID INTEGER, bikeNumber INTEGER);")
 c.execute(''' SELECT count(name) FROM sqlite_master WHERE type='table' AND name='REVIEWS' ''')
 if c.fetchone()[0] < 1:
-    c.execute("CREATE TABLE blogdata(userID INTEGER, topicID INTEGER PRIMARY KEY AUTOINCREMENT, location TEXT, rating INTEGER, content BLOB);")
+    c.execute("CREATE TABLE REVIEWS(userID INTEGER, bikeID TEXT, location TEXT, rating INTEGER, content BLOB);")
 c.execute(''' SELECT count(name) FROM sqlite_master WHERE type='table' AND name='BIKES' ''')
 if c.fetchone()[0] < 1:
-    c.execute("CREATE TABLE blogdata(userID INTEGER, topicID INTEGER PRIMARY KEY AUTOINCREMENT, location TEXT, rating INTEGER, content BLOB);")
+    c.execute("CREATE TABLE BIKES(bikeNumber INTEGER PRIMARY KEY AUTOINCREMENT, city TEXT, country TEXT, bikeID TEXT, name TEXT, latitude INTEGER, longitude INTEGER);")
 
 #-----------------------------------------------------------------
 
